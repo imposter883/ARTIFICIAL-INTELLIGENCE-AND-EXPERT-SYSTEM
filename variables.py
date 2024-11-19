ea1e731ab91c055e4c1e9c85855a503bf557cfae
Abdullah Shoab <imshoab883@gmail.com>
2024-11-20 00:53:04 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,35 @@
+#stirng data type
+x ="abcd"
+print(x)
+print(type(x))
+
+#int data type
+a = 5
+print(a)
+print(type(a))
+
+#float data type
+b=3.400
+print(b)
+print(type(b))
+
+#lsit
+c = {"apple","mango","banana"}
+print(c)
+print(type(c))
+
+#tupple
+abc = ("apple","banana","mango")
+print(abc)
+print(type(abc))
+
+#range
+d= range(6)
+for i in d:
+    print(d)
+    print(type(d))
+
+#set
+alphabet = {"a","b","c"}
+print(alphabet)
+print(type(alphabet))
